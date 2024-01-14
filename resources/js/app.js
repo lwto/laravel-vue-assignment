c633b97e8f5dcdd5063301637165807e9f1cce69
lwto <linwathanoo227@gmail.com>
2024-01-14 18:37:06 +0630

    vue and tailwind setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
-import './bootstrap';
+import { createApp } from "vue";
+import Test from './components/Test.vue';
+
+const app = createApp()
+
+app.component('test', Test)
+
+app.mount('#app')
---
+++ resources/js/components/Test.vue
@@ -0,0 +1,6 @@
+<script setup>
+
+</script>
+<template>
+    <h1 class="text-4xl">Testing...</h1>
+</template>
